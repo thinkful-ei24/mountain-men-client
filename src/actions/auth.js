09238da3c87faa7f4485391a749dd39f595b515c
Colin Rupp <colin@thinkful.com>
2018-12-04 13:26:23 -0800

    adds auth actions and utility functions for /actions/auth.js

---
+++ src/actions/auth.js
@@ -0,0 +1,113 @@
+import jwtDecode from 'jwt-decode';
+import {Submission Error} from 'redux-form';
+
+import {API_BASE_URL} from '../config.js';
+import {saveAuthToken, clearAuthToken} from '../local-storage.js';
+import {normalizeResponseErrors} from './utils.js';
+
+export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
+export const setAuthToken = (authToken) => ({
+  type: SET_AUTH_TOKEN,
+  authToken
+});
+
+export const CLEAR_AUTH = 'CLEAR_AUTH';
+export const clearAuth = () => ({
+  type: CLEAR_AUTH
+});
+
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const authRequest = () => ({
+  type: AUTH_REQUEST,
+});
+
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const authSuccess = (currentUser) => ({
+  type: AUTH_SUCCESS,
+  currentUser
+});
+
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const AuthError = (error) => ({
+  type: AUTH_ERROR,
+  error
+})
+
+const storeAuthInfo = (authToken, dispatch) => {
+  const decodeToken = jwtDecode(authToken);
+  dispatch(setAuthToken(authToken));
+  dispatch(authSuccess(decodedToken.user));
+  saveAuthToken(authToken);
+};
+
+export const login = (username, password) => dispatch => {
+  dispatch(authRequest());
+  return fetch(`${API_BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    bosy: JSON.stringify({
+      username,
+      password
+    })
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(({authToken}) => {
+      storeAuthInfo(authToken, dispatch)
+    })
+    .catch(err => {
+      const {code} = err;
+      const message =
+        code === 401 ? 'Incorrect username or password' : 'Unable to login, please try again';
+      dispatch(authError(err));
+      return Promise.reject(
+        new SubmissionError({
+          _error: message
+        })
+      )
+    })
+}
+
+export const refreshAuthToken = () => (dispatch, getState) => {
+  dispatch(authRequest());
+  const authToken = getState().auth.authToken;
+  return fetch(`${API_BASE_URL}/auth/refresh`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(({authToken}) => storeAuthInfo(authToken, dispatch))
+    .catch(err => {
+      dispatch(authError(err));
+      dispatch(clearAuth());
+      clearAuthToken(authToken);
+    });
+};
+
+export const registerUser = user => dispatch => {
+  return fetch(`${API_BASE_URL}/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .catch(err => {
+      dispatch(authError(err));
+      const {reason, message, location} = err;
+      if (reason === 'ValidationError') {
+        return Promise.reject(
+          new SubmissionError({
+            [location]: message
+          })
+        ).catch(err => dispatch(authError(err)))
+      }
+    });
+};
