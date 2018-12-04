0dd9667253165797ca9c6a50c53fb46d4fa3ba93
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-12-04 11:40:07 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import LandingPage from './pages/landingPage.js';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import LandingPage from "./pages/landingPage.js";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingPage />
+        <BrowserRouter>
+          <Route exact path="/" component={LandingPage} />
+        </BrowserRouter>
       </div>
     );
   }
