5263a6530700be6437d493c2792d1e37251d7181
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-12-04 13:16:45 -0700

    added navbar menu

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,20 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ShowNav from "./ShowNav";
 
-export default function HeaderBar() {
-  return (
-    <header>
-      <nav>{/** nav component */}</nav>
-      <ul>
-        <li>
-          <span>App Name</span>
-        </li>
-        <li>
-          <button>Sign Up</button>
-        </li>
-        <li>
-          <button>Log In</button>
-        </li>
-      </ul>
-    </header>
-  );
+export default class HeaderBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { click: false };
+  }
+
+  render() {
+    let show;
+    if (this.state.click) {
+      show = <ShowNav />;
+    }
+
+    return (
+      <header>
+        <button
+          onClick={e => {
+            this.setState({ click: !this.state.click });
+          }}
+        >
+          Show Nav
+        </button>
+        {show}
+        <ul>
+          <li>
+            <Link to="/">
+              <span>App Name</span>
+            </Link>
+          </li>
+          <li>
+            <button>Sign Up</button>
+          </li>
+          <li>
+            <button>Log In</button>
+          </li>
+        </ul>
+      </header>
+    );
+  }
 }
---
+++ src/components/ShowNav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ShowNav() {
+  return (
+    <div>
+      <span>Username</span>
+      <ul>
+        <li>Account</li>
+        <li>View Bids</li>
+        <li>Past Transactions</li>
+      </ul>
+      <ul>
+        <li>Help</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  );
+}
