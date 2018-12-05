cd947c2dd061ec4607ae420b689b00890a908682
Colin Rupp <colin@thinkful.com>
2018-12-05 09:14:35 -0800

    removes code breaking semi-colon

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -102,7 +102,7 @@ export const registerUser = user => dispatch => {
     .then(res => res.json())
     .then(res => {
       dispatch(login({email: res.email, password: res.password}))
-    });
+    })
     .catch(err => {
       dispatch(authError(err));
       const {reason, message, location} = err;
