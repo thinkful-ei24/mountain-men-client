49f8524e75dcc975e0541e8a5efbfbd13dbe7370
Colin Rupp <colin@thinkful.com>
2018-12-06 13:23:39 -0800

    adds past jobs to user dashboard

--- src/components/userProfileComponent.js
+++ src/components/userProfileComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PostJobForm from './PostJobForm';
+import PastJobs from './pastJobs.js';
 
 export function ProfileComponent(props) {
 
   return (
     <div id='profile-component'>
         <PostJobForm />
+        <PastJobs />
     </div>
   )
 }
