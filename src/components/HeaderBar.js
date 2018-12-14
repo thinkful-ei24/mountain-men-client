0ec7e32b1f8648cc3324368137a66cc6aa310b56
Alex <0alexgutierrez@gmail.com>
2018-12-14 14:37:19 -0700

    added profile/dashboard button

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -4,39 +4,59 @@ import { connect } from "react-redux";
 import { clearAuth } from "../actions/auth";
 import { clearAuthToken } from "../local-storage";
 import { updateView } from "../actions/view";
-import ShowNav from "./ShowNav";
-import logoImg from '../logo.png';
-require('../css/headerbar.css');
+import { withRouter } from "react-router";
+import logoImg from "../logo.png";
+require("../css/headerbar.css");
 
 export class HeaderBar extends React.Component {
-  state = { click: false };
-
   render() {
     let logout = () => {
       this.props.dispatch(clearAuth());
       clearAuthToken();
     };
 
+    let dashboardProfileButton;
+    if (this.props.location.pathname === "/profile") {
+      dashboardProfileButton = (
+        <Link to="/dashboard">
+          <button
+            id="profile-btn"
+            className="nav-btn"
+            onClick={() => {
+              this.props.dispatch(updateView("default"));
+            }}
+          >
+            Dashboard
+          </button>
+        </Link>
+      );
+    } else {
+      dashboardProfileButton = (
+        <Link to="/profile">
+          <button
+            id="profile-btn"
+            className="nav-btn"
+            onClick={() => {
+              this.props.dispatch(updateView("default"));
+              console.log(this.props.location);
+            }}
+          >
+            Profile
+          </button>
+        </Link>
+      );
+    }
+
     let showButtons;
     if (this.props.loggedIn) {
       showButtons = (
         <>
-          <li className="show-nav">
-            <Link to="/profile">
-                <button id='profile-btn'
-                  className='nav-btn'
-                  onClick={() => {
-                    this.props.dispatch(updateView("default"));
-                  }}
-                >
-                  Profile
-                </button>
-              </Link>
-          </li>
+          <li className="show-nav">{dashboardProfileButton}</li>
           <li className="logout">
             <Link to="/">
-              <button id='logout-btn'
-                className='nav-btn'
+              <button
+                id="logout-btn"
+                className="nav-btn"
                 type="button"
                 onClick={() => {
                   logout();
@@ -53,12 +73,12 @@ export class HeaderBar extends React.Component {
         <>
           <li className="sign-up">
             <Link to="/register">
-              <button className='nav-btn'>Sign Up</button>
+              <button className="nav-btn">Sign Up</button>
             </Link>
           </li>
           <li>
             <Link to="/login">
-              <button className='nav-btn'>Log In</button>
+              <button className="nav-btn">Log In</button>
             </Link>
           </li>
         </>
@@ -66,12 +86,12 @@ export class HeaderBar extends React.Component {
     }
 
     return (
-      <div id='header-bar'>
+      <div id="header-bar">
         <ul className="header">
           <li className="logo">
             <Link className="link-logo" to="/dashboard">
-              <img id='logo-img' src={logoImg} alt='logo'/>
-              <h1 id='title'>Truck'd</h1>
+              <img id="logo-img" src={logoImg} alt="logo" />
+              <h1 id="title">Truck'd</h1>
             </Link>
           </li>
           {showButtons}
@@ -85,4 +105,4 @@ const mapStateToProps = state => ({
   loggedIn: state.auth.currentUser !== null
 });
 
-export default connect(mapStateToProps)(HeaderBar);
+export default withRouter(connect(mapStateToProps)(HeaderBar));
