20cd7481c3fdb7c573ee0939f4c8d17430a05a5f
Shane Lupton <slupton89@gmail.com>
2018-12-20 16:27:32 -0700

    redirect fix #247
    https://www.youtube.com/watch?v=dQw4w9WgXcQ

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 require('../css/loginpage.css');
 
-export function LoginPage() {
+export function LoginPage(props) {
+  if(props.loggedIn) {
+    return <Redirect to='/dashboard'></Redirect>
+  }
   return (
     <div id='login-container' className='animated fadeIn'>
       <Link to='/'><div id='bg'></div></Link>
