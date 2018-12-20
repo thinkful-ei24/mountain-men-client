1d2fcddeadd004a7148621854abdefebed9dfc8d
Aaron Whitehead <whitehead.aaron@live.com>
2018-12-20 15:45:17 -0700

    fixed glitch where wrong information was getting found

--- src/components/DriverAcceptedBids.js
+++ src/components/DriverAcceptedBids.js
@@ -23,16 +23,12 @@ export default class DriverAcceptedBids extends React.Component {
     });
 
     let jobs = this.props.props.jobs.jobs.filter(item => {
-      return item.accepted;
+      return item.accepted && !item.completed;
     });
 
     driverBids.forEach(item => {
       jobs.forEach(job => {
-        if (
-          job.id === item.jobId &&
-          !job.completed &&
-          job.acceptedUserId === item.userId
-        ) {
+        if (job.id === item.jobId && job.acceptedUserId === item.userId) {
           item.accepted = job.accepted;
           item.completed = job.completed;
           item.jobDescription = job.description;
