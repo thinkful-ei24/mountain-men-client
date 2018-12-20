74626417c0f9d35423b08fe2edb880ef5187e719
Colin Rupp <colin@thinkful.com>
2018-12-20 08:42:51 -0800

    adds map styles

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -83,9 +83,10 @@ export class MapContainer extends React.Component {
       return <div>loading...</div>;
       } else if (this.props.center !== {}) {
         const style = {
-          width: '100%',
+          width: '80%',
           height: '70vh'
         }
+
 
 
         const jobs = this.props.jobs.filter(job => {
@@ -117,6 +118,220 @@ export class MapContainer extends React.Component {
               style={style}
               zoom={12}
               initialCenter={this.props.center}
+              styles={[
+                {
+                          "featureType": "all",
+                          "elementType": "geometry.fill",
+                  "stylers": [
+                    {
+                                      "color": "#ebebeb"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "landscape.man_made",
+                          "elementType": "geometry.fill",
+                  "stylers": [
+                    {
+                                      "color": "#d6d2cc"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "poi",
+                          "elementType": "geometry.fill",
+                  "stylers": [
+                    {
+                                      "color": "#8d867c"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "road.highway",
+                          "elementType": "geometry.fill",
+                  "stylers": [
+                    {
+                                      "color": "#8b1b41"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "road.highway",
+                          "elementType": "geometry.stroke",
+                  "stylers": [
+                    {
+                                      "color": "#8b1b41"
+
+                    },
+                    {
+                                      "lightness": "50"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "road.arterial",
+                          "elementType": "geometry.fill",
+                  "stylers": [
+                    {
+                                      "color": "#fcd27f"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "road.arterial",
+                          "elementType": "geometry.stroke",
+                  "stylers": [
+                    {
+                                      "color": "#fcd27f"
+
+                    },
+                    {
+                                      "lightness": "50"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "water",
+                          "elementType": "geometry.fill",
+                  "stylers": [
+                    {
+                                      "color": "#12202f"
+
+                    },
+                    {
+                                      "gamma": "2.00"
+
+                    }
+
+                  ]
+
+                  },
+                {
+                          "featureType": "water",
+                          "elementType": "labels.text.fill",
+                  "stylers": [
+                    {
+                                      "lightness": "100"
+
+                    }
+
+                  ][
+    {
+        "featureType": "all",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ebebeb"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d6d2cc"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#8d867c"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#8b1b41"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#8b1b41"
+            },
+            {
+                "lightness": "50"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fcd27f"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fcd27f"
+            },
+            {
+                "lightness": "50"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#12202f"
+            },
+            {
+                "gamma": "2.00"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "lightness": "100"
+            }
+        ]
+    }
+]
+
+                }
+
+              ] }
             >
 
             {markers}
